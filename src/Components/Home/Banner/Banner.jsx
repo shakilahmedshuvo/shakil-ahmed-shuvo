ad1dbf993d67fc89b21c41859e00cfb9c17d4519
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-28 19:22:13 +0600

    add banner fix

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import { TbDownload } from "react-icons/tb";
-import { FaPhoneAlt } from "react-icons/fa";
+import { FiPhone } from "react-icons/fi";
 
 const Banner = () => {
 
@@ -20,42 +20,51 @@ const Banner = () => {
                             I am a junior web developer.Who can provide full service of web development, specializing in HTML, CSS ,JAVASCRIPT, BOOTSTRAP, TAILWIND CSS, REACT, NODE JS ,EXPRESS JS, FIREBASE & MONGO DB. Also build custom responsive websites for clients.
                         </p>
                         <div
-                            className="grid grid-cols-2 sm:grid-cols-2 lg:gap-4 ">
-                            <a
-                                href="../../../../public/Resume of  Shakil Hossain jr developer.pdf"
-                                className="flex btn bg-blue-500 text-white hover:text-black hover:bg-blue-400">
-                                <TbDownload className="text-3xl font-bold" /> Download Resume
-                            </a>
-                            {/* modal section start */}
-                            <div>
-                                <button
-                                    className="btn btn-outline border-2 border-blue-500 btn-wide text-blue-500 font-bold"
-                                    onClick={() => window.my_modal_1.showModal()}>
-                                    <FaPhoneAlt className="text-xl font-bold" /> Contact
-                                </button>
-                                <dialog
-                                    id="my_modal_1"
-                                    className="modal">
-                                    <form
-                                        method="dialog"
-                                        className="modal-box">
-                                        <h3
-                                            className="font-bold text-lg">
-                                            Hello!
-                                        </h3>
-                                        <p
-                                            className="py-4">
-                                            Press ESC key or click the button below to close
-                                        </p>
-                                        <div
-                                            className="modal-action">
-                                            <button
-                                                className="btn">
-                                                Close
-                                            </button>
-                                        </div>
-                                    </form>
-                                </dialog>
+                            className="lg:block hidden">
+                            <div
+                                className="grid lg:grid-cols-2 sm:grid-cols-2 gap-4">
+                                <a
+                                    href="../../../../public/Resume of  Shakil Hossain jr developer.pdf"
+                                    className="flex btn bg-blue-500 text-white hover:bg-blue-400 h-full">
+                                    <TbDownload className="text-3xl font-bold" /> Download Resume
+                                </a>
+                                {/* modal section start */}
+                                <div>
+                                    <button
+                                        className="btn btn-outline border-2 border-blue-500 w-full text-blue-500 font-bold"
+                                        onClick={() => window.my_modal_1.showModal()}>
+                                        <FiPhone className="text-xl font-bold" /> Contact
+                                    </button>
+                                    <dialog
+                                        id="my_modal_1"
+                                        className="modal">
+                                        <form
+                                            method="dialog"
+                                            className="modal-box">
+                                            <h3
+                                                className="font-bold text-2xl text-blue-500 text-center">
+                                                Contact Info
+                                            </h3>
+                                            <p
+                                                className="py-6 text-center font-bold">
+                                                Email Adders :
+                                                <br />
+                                                Shakilahmedshuvo612@gmail.com
+                                                <br />
+                                                Phone :
+                                                <br />
+                                                +880 190299 1164
+                                            </p>
+                                            <span
+                                                className="modal-action">
+                                                <button
+                                                    className="btn bg-blue-500 text-white hover:text-black hover:bg-blue-400 mx-auto">
+                                                    Close
+                                                </button>
+                                            </span>
+                                        </form>
+                                    </dialog>
+                                </div>
                             </div>
                         </div>
                         {/* modal section end */}
@@ -68,8 +77,57 @@ const Banner = () => {
                         src="https://img.freepik.com/free-vector/programmer-concept-illustration_114360-2417.jpg?w=740&t=st=1687953702~exp=1687954302~hmac=2d0612b0b1da7ef90ae421365c4f5af604241978e7ba567ea5013eb9cc2fe9d7"
                         alt="" />
                 </div>
+                <div
+                   className="block sm:hidden">
+                    <div
+                        className="grid lg:grid-cols-2 sm:grid-cols-2 gap-4 px-4">
+                        <a
+                            href="../../../../public/Resume of  Shakil Hossain jr developer.pdf"
+                            className="flex btn bg-blue-500 text-white hover:bg-blue-400 h-full">
+                            <TbDownload className="text-3xl font-bold" /> Download Resume
+                        </a>
+                        {/* modal section start */}
+                        <div className="">
+                            <button
+                                className="btn btn-outline border-2 border-blue-500 w-full text-blue-500 font-bold"
+                                onClick={() => window.my_modal_1.showModal()}>
+                                <FiPhone className="text-xl font-bold" /> Contact
+                            </button>
+                            <dialog
+                                id="my_modal_1"
+                                className="modal">
+                                <form
+                                    method="dialog"
+                                    className="modal-box">
+                                    <h3
+                                        className="font-bold text-2xl text-blue-500 text-center">
+                                        Contact Info
+                                    </h3>
+                                    <p
+                                        className="py-6 text-center font-bold">
+                                        Email Adders :
+                                        <br />
+                                        Shakilahmedshuvo612@gmail.com
+                                        <br />
+                                        Phone :
+                                        <br />
+                                        +880 190299 1164
+                                    </p>
+                                    <span
+                                        className="modal-action">
+                                        <button
+                                            className="btn bg-blue-500 text-white hover:text-black hover:bg-blue-400 mx-auto">
+                                            Close
+                                        </button>
+                                    </span>
+                                </form>
+                            </dialog>
+                        </div>
+                    </div>
+                </div>
+                {/* modal section end */}
             </div>
-        </div >
+        </div>
     );
 };
 
