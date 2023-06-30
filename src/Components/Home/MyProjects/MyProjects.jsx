93fcd95d9b075f0c534affdd0aa9408245ba2943
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-07-01 01:53:22 +0600

    add json file

--- src/Components/Home/MyProjects/MyProjects.jsx
+++ src/Components/Home/MyProjects/MyProjects.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../Shared/SectionTitle";
+import MyProjectsCard from "./MyProjectsCard";
 
 const MyProjects = () => {
+    const [data, setData] = useState([]);
+
+    // load the data
+    useEffect(() => {
+        fetch('project.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, []);
     return (
         <div
             className="max-w-7xl mx-auto">
             <SectionTitle
                 heading={"- My Project's -"}
             ></SectionTitle>
-            <div
-                className="grid lg:grid-cols-3 gap-5 my-10">
-                
+            {/* map the data */}
+            <div>
+                {
+                    data.map(data => <MyProjectsCard
+                        key={data.id}
+                        data={data}
+                    ></MyProjectsCard>)
+                }
             </div>
         </div>
     );
---
+++ src/Components/Home/MyProjects/MyProjectsCard.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const MyProjectsCard = ({ data }) => {
+    const { name, shortInfo, img, liveLink, clientLink, serverLink, feature, techIcon } = data;
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyProjectsCard;
