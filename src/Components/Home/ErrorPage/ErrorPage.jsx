a0612898880ce6fd85c8b9bc95dfa2090d44120c
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-30 17:44:23 +0600

    change the 404 pae

--- src/Components/Home/ErrorPage/ErrorPage.jsx
+++ src/Components/Home/ErrorPage/ErrorPage.jsx
@@ -7,26 +7,19 @@ const ErrorPage = () => {
     return (
         <>
             <NavBar />
-            <div className='text-center pt-20'>
+            <div className='text-center pt-3'>
                 <div className='lg:w-[50%] sm:w-full mx-auto'>
                     <img
-                        className='mx-auto lg:w-[100%] sm:w-[100%] rounded-lg pt-6'
-                        src="https://img.freepik.com/premium-vector/error-404-banner-vector-illustration_18591-530.jpg?w=996"
-                        alt="" />
+                        className='mx-auto lg:w-[60%] sm:w-[100%] rounded-lg pt-6'
+                        src="https://img.freepik.com/premium-vector/error-404-illustration_585024-2.jpg?w=740"
+                        alt="404 page not found" />
                 </div>
-                {/* <h2
-                    className='font-extrabold text-5xl'>
-                    <span
-                        className='text-error'>
-                        Error
-                    </span> {status || 404}
-                </h2> */}
                 <p
                     className='font-bold text-xl py-2'>
                     {error?.message}
                 </p>
                 <p 
-                className="py-2">
+                className="py-4">
                     <Link
                         to='/'
                         className='no-underline font-extrabold btn btn-warning rounded-lg btn-wide '>
