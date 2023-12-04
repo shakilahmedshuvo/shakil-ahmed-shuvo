2487836c185be22fb90ae55e4c8356b5e82e2762
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-12-04 11:41:59 +0600

    add some icons

--- src/Components/Shared/SectionTitle.jsx
+++ src/Components/Shared/SectionTitle.jsx
@@ -1,10 +1,10 @@
 const SectionTitle = ({ heading }) => {
     return (
-        <div>
+        <div className="mx-8 lg:mx-0">
             <div
                 className="md:w-4/12 mx-auto text-center">
                 <h3
-                    className="text-3xl uppercase border-y-4 py-4">
+                    className="text-xl lg:text-3xl uppercase border-y-2 lg:border-y-4 lg:py-4 py-3">
                     {heading}
                 </h3>
             </div>
